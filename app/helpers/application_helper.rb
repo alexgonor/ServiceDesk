366078e61936de408082fdac6cae61edf6fb5faf
Radko-R <crius@protonmail.com>
2018-02-09 19:34:12 +0200

    gem Device User

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def bootstrap_class_for(name)
+    { success: "alert-success",
+      error: "alert-danger",
+      danger: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info"
+    }[name.to_sym] || name
+  end
 end
+
+
