c6d5ccbf3d7f94d9392c49df73894368b455d061
crius <crius@protonmail.com>
2018-02-22 17:25:04 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-root to: "tickets#index"
 Rails.application.routes.draw do
+  root to: "tickets#index"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
