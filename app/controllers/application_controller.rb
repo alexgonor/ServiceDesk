d48d24a02a178d873dafa7b69f609a4afb882fa0
Vladyslav Volkov <vva_05@ukr.net>
2018-03-08 15:10:00 +0200

    Little Corrections

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
@@ -15,18 +13,4 @@ class ApplicationController < ActionController::Base
   def initialize_current_user
     User.current = current_user
   end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:username, :email, :password, :password_confirmation,
-               :remember_me, :avatar, :avatar_cache)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:username, :password, :password_confirmation,
-               :current_password, :avatar, :avatar_cache,
-               :remove_avatar)
-    end
-  end
-end
+ end
