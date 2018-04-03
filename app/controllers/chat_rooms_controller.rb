464507c41966f7d902d1ef45f1f0cec21b54ae0c
alexgonor <gonor.alexandr@gmail.com>
2018-04-03 19:39:53 +0300

    executor actions with ticket status

--- app/controllers/chat_rooms_controller.rb
+++
@@ -1,6 +0,0 @@
-class ChatRoomsController < ApplicationController
-  before_action :authenticate_user!
-  def show
-    @messages = Message.all
-  end
-end
--- app/models/message.rb
+++
@@ -1,4 +0,0 @@
-class Message < ApplicationRecord
-  belongs_to :user
-  # after_create_commit { BroadcastMessageJob.perform_later self }
-end
