9b88c33c7b4084934f08d554c98409825cb3a18a
alexgonor <gonor.alexandr@gmail.com>
2018-04-05 13:41:15 +0300

    add priority to ticket

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-gem "rails", "~> 5.1.4"
+gem "rails", "5.1.4"
 
 gem 'activeadmin'
 gem "active_admin_paranoia" , '~> 1.0.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180325111540) do
+ActiveRecord::Schema.define(version: 20180405073222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 20180325111540) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id", null: false
+    t.integer "priority"
     t.index ["user_id"], name: "index_tickets_on_user_id"
   end
 
