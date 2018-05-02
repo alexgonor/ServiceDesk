6c0f56d7bf7f68b6cb500dc2b431a8a373211992
Vladyslav Volkov <vva_05@ukr.net>
2018-05-02 14:04:01 +0300

    Using CKEditor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( filterrific/filterrific-spinner.gif )
+Rails.application.config.assets.precompile += %w( ckeditor/*)
